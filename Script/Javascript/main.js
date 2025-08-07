d89418cdfea14f466215f7c9696f14317c546961
daneelsjo <92335178+daneelsjo@users.noreply.github.com>
2025-08-07 20:58:54 +0200

    Update main.js

--- Script/Javascript/main.js
+++ Script/Javascript/main.js
@@ -28,7 +28,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
 
