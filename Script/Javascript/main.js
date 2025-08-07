6e59247d6245cd73217170b2e18287543a722a86
daneelsjo <92335178+daneelsjo@users.noreply.github.com>
2025-08-07 20:55:27 +0200

    Update main.js
    initializeApp stond er 2x in

--- Script/Javascript/main.js
+++ Script/Javascript/main.js
@@ -8,7 +8,6 @@ import {
 
 // 🔧 Vul hieronder je eigen Firebase-config in
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
